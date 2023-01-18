3d4ae200fb9c089f23759bf3e87e83dd6f0f54d4
NCenek2 <ceneknicholas@gmail.com>
2023-01-17 19:47:55 -0500

    Full Game Update

--- index.js
+++ index.js
@@ -1,67 +1,91 @@
-const buttonArray = ["green", "red", "yellow", "blue"];
+// Button Array to represent 0-3
+let buttonLength = $(".btn").length;
+const buttonArray = ["red", "green", "blue", "yellow"];
 
+// Global Variables
 let currentLevel = 1;
-let i = 0;
-let levelArray = [];
-let userArray = [];
+let index = 0;
+let gameChoices = [];
+let userChoices = [];
+let userPlaying;
 
 function myLoop() {
   setTimeout(function () {
-    const id = buttonArray[Math.floor(Math.random() * 4)];
-    handleOpacity(id);
-    levelArray.push(id);
-    console.log(levelArray, userArray);
-    i++;
-    if (i < currentLevel) {
+    // Controls Game Pick
+    const randomNumber = buttonArray[Math.floor(Math.random() * buttonLength)];
+    handleOpacity(randomNumber);
+    // Push the value into the game array
+    gameChoices.push(randomNumber);
+    // console.log(gameChoices, userChoices);
+    // Increase Index to Include More Choices Depending on Level
+    index++;
+    if (index < currentLevel) {
       myLoop();
+    } else {
+      // Allows user to Click on Buttons After Test
+      userPlaying = true;
     }
   }, 1000);
 }
-
-myLoop();
 
 function handleOpacity(id) {
   $(`#${id}`).animate({ opacity: "25%" }).animate({ opacity: "100%" });
 }
 
-function handleSelections(levelArr, userArr) {
-  for (let i = 0; i < levelArr.length; i++) {
+function compareChoices(levelArr, userArr) {
+  for (let i = 0; i < userArr.length; i++) {
     if (levelArr[i] != userArr[i]) {
       return false;
     }
   }
-
   return true;
 }
+
+function resetLevelVariables() {
+  index = 0;
+  gameChoices = [];
+  userChoices = [];
+}
+
+function handleLevelChange(level, text) {
+  $("h1").text(`${text}`);
+  resetLevelVariables();
+  setTimeout(() => {
+    $("h1").text(`Level ${level}`);
+    myLoop();
+  }, 1000);
+  userPlaying = false;
+}
+
 // Handle Opacity Change
 $(".btn").click(function (event) {
-  $(`#${event.target.id}`)
-    .animate({ opacity: "50%" })
-    .animate({ opacity: "100%" });
-  userArray.push(`${event.target.id}`);
-  if (
-    levelArray.length == userArray.length &&
-    levelArray.length > 0 &&
-    userArray.length > 0
-  ) {
-    if (handleSelections(levelArray, userArray)) {
-      $("h1").text(`Level ${currentLevel + 1}`);
-      currentLevel++;
-      myLoop(currentLevel);
-      i = 0;
-      levelArray = [];
-      userArray = [];
+  if (userPlaying) {
+    // Local Variables
+    let text = "";
+    const lossText = "Try Again!";
+
+    // Push to User Choices on Clicks
+    handleOpacity(event.target.id);
+    userChoices.push(`${event.target.id}`);
+
+    if (gameChoices.length == userChoices.length && gameChoices.length > 0) {
+      if (compareChoices(gameChoices, userChoices)) {
+        currentLevel++;
+        text = `Level ${currentLevel}`;
+        handleLevelChange(currentLevel, text);
+      } else {
+        currentLevel = 1;
+        handleLevelChange(currentLevel, lossText);
+        // Handle Loss
+      }
     } else {
-      $("h1").text(`Try Again!`);
-      currentLevel = 1;
-      i = 0;
-      levelArray = [];
-      userArray = [];
-      setTimeout(() => {
-        myLoop(1);
-        $("h1").text(`Level ${currentLevel}`);
-      }, 3000);
+      // Handles Incremental User Clicks
+      if (!compareChoices(gameChoices, userChoices)) {
+        currentLevel = 1;
+        handleLevelChange(currentLevel, lossText);
+      }
     }
   }
-  console.log(levelArray, userArray);
 });
+
+myLoop();
